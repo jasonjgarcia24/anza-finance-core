942ae325d7142fb948f4b23032ddee199dc56ab6
jjgarcia.eth <jason.garcia24@gmail.com>
2023-05-12 23:58:24 -0500

    Removal of js.

--- utils/db/archive/create_contracts.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE contracts;
-
-CREATE TABLE
-    IF NOT EXISTS contracts.borrowers(
-        accountAddress VARCHAR(42) NOT NULL,
-        tokenContractAddress VARCHAR(42) NOT NULL,
-        tokenId VARCHAR(78) NOT NULL,
-        contractAddress VARCHAR(42)
-    );
